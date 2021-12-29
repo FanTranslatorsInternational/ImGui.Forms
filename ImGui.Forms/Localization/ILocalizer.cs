57364bc76d16c0def9f3cd711143a318f748f9ba
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-12-29 22:36:19 +0100

    Add CurrentLocale to localizer interface;

--- ImGui.Forms/Localization/ILocalizer.cs
+++ ImGui.Forms/Localization/ILocalizer.cs
@@ -2,6 +2,8 @@
 {
     public interface ILocalizer
     {
+        string CurrentLocale { get; }
+
         void ChangeLocale(string locale);
 
         string Localize(string name, params object[] args);

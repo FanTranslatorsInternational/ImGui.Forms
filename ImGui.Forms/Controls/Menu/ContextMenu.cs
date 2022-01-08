5fcf3fa8cee417228e1d2fdb3885675be140e4b6
onepiecefreak3 <realonepiecefreak@gmx.de>
2022-01-09 00:22:34 +0100

    Add Show event to ContextMenu;

--- ImGui.Forms/Controls/Menu/ContextMenu.cs
+++ ImGui.Forms/Controls/Menu/ContextMenu.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ImGuiNET;
 
 namespace ImGui.Forms.Controls.Menu
 {
     public class ContextMenu
     {
+        private bool _isOpen;
+
         /// <summary>
         /// The Id for this component.
         /// </summary>
@@ -12,19 +15,42 @@ namespace ImGui.Forms.Controls.Menu
 
         public IList<MenuBarItem> Items { get; } = new List<MenuBarItem>();
 
+        #region Events
+
+        public event EventHandler Show;
+
+        #endregion
+
         public void Update()
         {
             ImGuiNET.ImGui.PushID(Id);
 
+            var isOpenLocal = false;
             if (ImGuiNET.ImGui.BeginPopupContextItem(Id.ToString(), ImGuiPopupFlags.NoOpenOverExistingPopup | ImGuiPopupFlags.MouseButtonRight))
             {
+                isOpenLocal = true;
+
+                if (!_isOpen)
+                {
+                    _isOpen = true;
+                    OnShow();
+                }
+
                 foreach (var item in Items)
                     item.Update();
 
                 ImGuiNET.ImGui.EndPopup();
             }
 
+            if (_isOpen && !isOpenLocal)
+                _isOpen = false;
+
             ImGuiNET.ImGui.PopID();
+        }
+
+        private void OnShow()
+        {
+            Show?.Invoke(this, new EventArgs());
         }
     }
 }

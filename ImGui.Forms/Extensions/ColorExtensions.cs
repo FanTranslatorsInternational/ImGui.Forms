a8f852c35017f25a9b3dcb484956a5e862d4c7b1
onepiecefreak3 <realonepiecefreak@gmx.de>
2022-01-11 23:00:22 +0100

    Add ToColor extension for uint;

--- ImGui.Forms/Extensions/ColorExtensions.cs
+++ ImGui.Forms/Extensions/ColorExtensions.cs
@@ -2,11 +2,16 @@
 
 namespace ImGui.Forms.Extensions
 {
-    static class ColorExtensions
+    public static class ColorExtensions
     {
         public static uint ToUInt32(this Color c)
         {
             return (uint)((c.A << 24) | (c.B << 16) | (c.G << 8) | c.R);
+        }
+
+        public static Color ToColor(this uint value)
+        {
+            return Color.FromArgb((byte)(value >> 24), (byte)value, (byte)(value >> 8), (byte)(value >> 16));
         }
     }
 }

9124d643381141622f676d5b00679a3456a6582b
onepiecefreak3 <realonepiecefreak@gmx.de>
2022-05-07 14:42:28 +0200

    Add Panel component;

---
+++ ImGui.Forms/Controls/Panel.cs
@@ -0,0 +1,23 @@
+﻿using ImGui.Forms.Controls.Base;
+using ImGui.Forms.Models;
+using Veldrid;
+
+namespace ImGui.Forms.Controls
+{
+    public class Panel : Component
+    {
+        public Size Size { get; set; } = Size.Parent;
+
+        public Component Content { get; set; }
+
+        public override Size GetSize()
+        {
+            return Size;
+        }
+
+        protected override void UpdateInternal(Rectangle contentRect)
+        {
+            Content?.Update(contentRect);
+        }
+    }
+}

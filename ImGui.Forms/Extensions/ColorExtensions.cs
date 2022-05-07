58b86924fb65b7de50700d218919917b0912a1d2
onepiecefreak3 <realonepiecefreak@gmx.de>
2022-05-07 14:42:52 +0200

    Add color extensions for Vector4;

--- ImGui.Forms/Extensions/ColorExtensions.cs
+++ ImGui.Forms/Extensions/ColorExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Numerics;
 
 namespace ImGui.Forms.Extensions
 {
@@ -12,6 +13,16 @@ namespace ImGui.Forms.Extensions
         public static Color ToColor(this uint value)
         {
             return Color.FromArgb((byte)(value >> 24), (byte)value, (byte)(value >> 8), (byte)(value >> 16));
+        }
+
+        public static Vector4 ToVector4(this Color c)
+        {
+            return new Vector4(c.R / 255f, c.G / 255f, c.B / 255f, c.A / 255f);
+        }
+
+        public static Color ToColor(this Vector4 value)
+        {
+            return Color.FromArgb((int)(value.W * 255), (int)(value.X * 255), (int)(value.Y * 255), (int)(value.Z * 255));
         }
     }
 }

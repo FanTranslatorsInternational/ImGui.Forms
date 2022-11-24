76efdbfaa77382035cbbe11256d805c3cdf0d22b
onepiecefreak3 <realonepiecefreak@gmx.de>
2022-11-24 23:41:34 +0100

    Add base implementation for lists;

---
+++ ImGui.Forms/Controls/Lists/BaseList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ImGui.Forms.Controls.Base;
+using ImGui.Forms.Controls.Layouts;
+using Veldrid;
+using Size = ImGui.Forms.Models.Size;
+
+namespace ImGui.Forms.Controls.Lists
+{
+    public abstract class BaseList<TItem> : Component
+        where TItem : Component
+    {
+        public IList<TItem> Items { get; protected set; } = new List<TItem>();
+        public Alignment Alignment { get; set; } = Alignment.Vertical;
+
+        public Size Size { get; set; } = Size.Content;
+        public int ItemSpacing { get; set; }
+
+        public override Size GetSize() => Size;
+
+        protected override int GetContentWidth(int parentWidth, float layoutCorrection = 1)
+        {
+            var widths = Items.Select(x => x.GetWidth(parentWidth, layoutCorrection)).DefaultIfEmpty(Size.Width.IsParentAligned ? parentWidth : 0);
+
+            var totalWidth = Alignment == Alignment.Horizontal ?
+                widths.Sum() + Math.Max(0, Items.Count - 1) * ItemSpacing :
+                widths.Max();
+            return Math.Min(parentWidth, totalWidth);
+        }
+
+        protected override int GetContentHeight(int parentHeight, float layoutCorrection = 1)
+        {
+            var heights = Items.Select(x => x.GetHeight(parentHeight, layoutCorrection)).DefaultIfEmpty(Size.Height.IsParentAligned ? parentHeight : 0);
+
+            var totalHeight = Alignment == Alignment.Vertical ?
+                heights.Sum() + Math.Max(0, Items.Count - 1) * ItemSpacing :
+                heights.Max();
+            return Math.Min(parentHeight, totalHeight);
+        }
+
+        protected override void UpdateInternal(Rectangle contentRect)
+        {
+            var localItems = Items?.ToArray() ?? Array.Empty<Component>();
+
+            var listDimension = localItems.Sum(i => GetDimension(i, contentRect)) + Math.Max(0, localItems.Length - 1) * ItemSpacing;
+            var scrollableDimension = GetScrollableDimension(contentRect);
+
+            if (ImGuiNET.ImGui.BeginChild($"{Id}", contentRect.Size, false))
+            {
+                var scrollbarDimension = listDimension > scrollableDimension ? (int)ImGuiNET.ImGui.GetStyle().ScrollbarSize : 0;
+                var (scrollX, scrollY) = (-(int)ImGuiNET.ImGui.GetScrollX(), -(int)ImGuiNET.ImGui.GetScrollY());
+
+                var (x, y) = (0, 0);
+
+                if (ImGuiNET.ImGui.BeginChild($"{Id}_in", GetInnerSize(listDimension, contentRect)))
+                {
+                    for (var i = 0; i < localItems.Length; i++)
+                    {
+                        var itemWidth = localItems[i].GetWidth(contentRect.Width - (Alignment == Alignment.Vertical ? scrollbarDimension : 0));
+                        var itemHeight = localItems[i].GetHeight(contentRect.Height - (Alignment == Alignment.Horizontal ? scrollbarDimension : 0));
+
+                        ImGuiNET.ImGui.SetCursorPos(new Vector2(x, y));
+                        if (ImGuiNET.ImGui.BeginChild($"{Id}_itm{i}", new Vector2(itemWidth, itemHeight)))
+                            localItems[i].Update(new Rectangle(contentRect.X + x + scrollX, contentRect.Y + y + scrollY, itemWidth, itemHeight));
+
+                        ImGuiNET.ImGui.EndChild();
+
+                        if (Alignment == Alignment.Vertical)
+                            y += itemHeight + ItemSpacing;
+                        else
+                            x += itemWidth + ItemSpacing;
+                    }
+                }
+
+                ImGuiNET.ImGui.EndChild();
+            }
+
+            ImGuiNET.ImGui.EndChild();
+        }
+
+        private int GetDimension(Component component, Rectangle contentRect)
+        {
+            return Alignment == Alignment.Vertical
+                ? component.GetHeight(contentRect.Height)
+                : component.GetWidth(contentRect.Width);
+        }
+
+        private int GetScrollableDimension(Rectangle contentRect)
+        {
+            return Alignment == Alignment.Vertical
+                ? contentRect.Height
+                : contentRect.Width;
+        }
+
+        private Vector2 GetInnerSize(int listDimension, Rectangle contentRect)
+        {
+            return Alignment == Alignment.Vertical ?
+                new Vector2(contentRect.Width, listDimension) :
+                new Vector2(listDimension, contentRect.Height);
+        }
+    }
+}
--- ImGui.Forms/Controls/Lists/List.cs
+++ ImGui.Forms/Controls/Lists/List.cs
@@ -1,60 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using ImGui.Forms.Controls.Base;
-using ImGui.Forms.Models;
-using Veldrid;
+﻿using ImGui.Forms.Controls.Base;
 
 namespace ImGui.Forms.Controls.Lists
 {
-    public class List : Component
+    public class List : BaseList<Component>
     {
-        public IList<Component> Items { get; set; } = new List<Component>();
-
-        public int ItemSpacing { get; set; }
-
-        public bool HasBorder { get; set; }
-
-        public Size Size { get; set; }=Size.Parent;
-
-        public override Size GetSize()
-        {
-            return Size;
-        }
-
-        protected override void UpdateInternal(Rectangle contentRect)
-        {
-            var localItems = Items?.ToArray() ?? Array.Empty<Component>();
-            var listHeight = localItems.Sum(i => i.GetHeight(contentRect.Height)) + Math.Max(0, localItems.Length - 1) * ItemSpacing;
-
-            if (ImGuiNET.ImGui.BeginChild($"{Id}", new Vector2(contentRect.Width, contentRect.Height), HasBorder))
-            {
-                var scrollbarWidth = listHeight > contentRect.Height ? ImGuiNET.ImGui.GetStyle().ScrollbarSize : 0;
-                var scrollY = -(int)ImGuiNET.ImGui.GetScrollY();
-                var y = 0;
-
-                if (ImGuiNET.ImGui.BeginChild($"{Id}_in", new Vector2(contentRect.Width, listHeight)))
-                {
-                    for (var i = 0; i < localItems.Length; i++)
-                    {
-                        var itemWidth = localItems[i].GetWidth(contentRect.Width - (int) scrollbarWidth);
-                        var itemHeight = localItems[i].GetHeight(contentRect.Height);
-
-                        ImGuiNET.ImGui.SetCursorPos(new Vector2(0, y));
-                        if (ImGuiNET.ImGui.BeginChild($"{Id}_itm{i}", new Vector2(contentRect.Width - (int)scrollbarWidth, itemHeight)))
-                            localItems[i].Update(new Rectangle(contentRect.X, contentRect.Y + y + scrollY, itemWidth, itemHeight));
-
-                        ImGuiNET.ImGui.EndChild();
-
-                        y += itemHeight + ItemSpacing;
-                    }
-                }
-
-                ImGuiNET.ImGui.EndChild();
-            }
-
-            ImGuiNET.ImGui.EndChild();
-        }
     }
 }

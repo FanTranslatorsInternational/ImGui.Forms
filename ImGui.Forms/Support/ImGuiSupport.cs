2a32bc6e2ff12d90763174039384e602eb257624
onepiecefreak3 <realonepiecefreak@gmx.de>
2023-01-04 19:03:29 +0100

    Add dummy control for ImGui support;

---
+++ ImGui.Forms/Support/ImGuiSupport.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using ImGuiNET;
+
+namespace ImGui.Forms.Support
+{
+    static class ImGuiSupport
+    {
+        public static void Dummy(int id, Vector2 size)
+        {
+            ImGuiNET.ImGui.PushStyleColor(ImGuiCol.Button, 0);
+            ImGuiNET.ImGui.PushStyleColor(ImGuiCol.ButtonHovered, 0);
+            ImGuiNET.ImGui.PushStyleColor(ImGuiCol.ButtonActive, 0);
+
+            ImGuiNET.ImGui.Button($"##{id}", size);
+
+            ImGuiNET.ImGui.PopStyleColor(3);
+        }
+    }
+}

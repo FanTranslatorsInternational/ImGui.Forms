9a53a454ad9d000a86d0bc47bdf7a1a84efaf959
onepiecefreak3 <realonepiecefreak@gmx.de>
2023-01-08 21:14:10 +0100

    Add splitter component; FileFilter name localizable;

---
+++ ImGui.Forms/Controls/Splitter.cs
@@ -0,0 +1,22 @@
+﻿using ImGui.Forms.Controls.Base;
+using ImGui.Forms.Controls.Layouts;
+using ImGui.Forms.Models;
+using ImGuiNET;
+using Veldrid;
+
+namespace ImGui.Forms.Controls
+{
+    public class Splitter : Component
+    {
+        public Alignment Alignment { get; set; } = Alignment.Vertical;
+
+        public SizeValue Length { get; set; }
+
+        public override Size GetSize() => Alignment == Alignment.Horizontal ? new Size(Length, 1) : new Size(1, Length);
+
+        protected override void UpdateInternal(Rectangle contentRect)
+        {
+            ImGuiNET.ImGui.GetWindowDrawList().AddRectFilled(contentRect.Position, contentRect.Position + contentRect.Size, ImGuiNET.ImGui.GetColorU32(ImGuiCol.Border));
+        }
+    }
+}
--- ImGui.Forms/Modals/IO/FileFilter.cs
+++ ImGui.Forms/Modals/IO/FileFilter.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
+using ImGui.Forms.Localization;
 
 namespace ImGui.Forms.Modals.IO
 {
     public class FileFilter
     {
-        public string Name { get; }
+        public LocalizedString Name { get; }
         public IList<string> Extensions { get; } = new List<string>();
 
-        public FileFilter(string name, params string[] extensions)
+        public FileFilter(LocalizedString name, params string[] extensions)
         {
             Name = name;
             foreach (var ext in extensions)

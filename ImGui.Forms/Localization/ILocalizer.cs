e4fa2be54e6acbc9138c6d60ba76ff151d01df6e
onepiecefreak3 <realonepiecefreak@gmx.de>
2023-11-21 00:14:43 +0100

    Add more methods to ILocalizer;

--- ImGui.Forms/Localization/ILocalizer.cs
+++ ImGui.Forms/Localization/ILocalizer.cs
@@ -1,9 +1,15 @@
-﻿namespace ImGui.Forms.Localization
+﻿using System.Collections.Generic;
+
+namespace ImGui.Forms.Localization
 {
     public interface ILocalizer
     {
         string CurrentLocale { get; }
 
+        IList<string> GetLocales();
+
+        string GetLanguageName(string locale);
+        string GetLocaleByName(string name);
         void ChangeLocale(string locale);
 
         string Localize(string name, params object[] args);
